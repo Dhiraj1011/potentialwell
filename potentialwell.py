dd88f9ce560d2535d5d3b05fac1242f3ff3ac496
Dhiraj1011 <159097901+Dhiraj1011@users.noreply.github.com>
2024-02-19 22:19:17 +0530

    Update potentialwell.py

--- potentialwell.py
+++ potentialwell.py
@@ -21,7 +21,7 @@ plt.plot(x,(psi(x,n))**2,'--',label=rf'$\psi_{n}$')
 
 plt.ylabel('$\psi(x)^2$')
 plt.xlabel('x')
-plt.xlim(0,4)
+plt.xlim(0,a)
 plt.ylim(bottom=0)
 plt.savefig('graph.jpg')
 st.image('graph.jpg')
