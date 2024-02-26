68772d838c01cea971959ecc35083f8c8603e4af
Dhiraj1011 <159097901+Dhiraj1011@users.noreply.github.com>
2024-02-26 08:28:27 +0530

    Update potentialwell.py

--- potentialwell.py
+++ potentialwell.py
@@ -16,14 +16,17 @@ def psi(x, n):
 n=st.slider('Enter the state',1,100)
 st.text(n)
 plt.subplot(2,1,1)
-plt.plot(x,(psi(x,n)))
+plt.plot(x,(psi(x,n)),label=rf'$\psi_{n}$')
+plt.xlim(0,a)
 plt.axhline(0,color='black')
 plt.subplot(2,1,2)
-plt.plot(x,(psi(x,n))**2,'--',label=rf'$\psi_{n}$')
+plt.plot(x,(psi(x,n))**2,label=rf'$\psi_{n}$')
 plt.ylabel('$\psi(x)^2$')
 plt.xlabel('x')
 plt.xlim(0,a)
 plt.ylim(bottom=0)
 plt.savefig('graph.jpg')
 st.image('graph.jpg')
+
+
 
