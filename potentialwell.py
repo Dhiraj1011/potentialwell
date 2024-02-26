7083a93e492f4824d1de35fd06e61d7ab24f722b
Dhiraj1011 <159097901+Dhiraj1011@users.noreply.github.com>
2024-02-26 09:39:00 +0530

    Update potentialwell.py

--- potentialwell.py
+++ potentialwell.py
@@ -7,7 +7,7 @@ st.title('Quantum Mechanics')
 st.header('1-D Potential Well',divider='rainbow')
 
 a=st.slider('Enter the length of well',1,20)
-x=np.linspace(-a,a,10000)
+x=np.linspace(0,a,10000)
 
 def psi(x, n):
     y = np.sqrt(2 / a) * np.sin((n * np.pi * x) / (a))
@@ -28,3 +28,19 @@ plt.ylim(bottom=0)
 plt.savefig('graph.jpg')
 st.image('graph.jpg')
 
+def psi1(x, n):
+    y = np.sqrt(1 / (a)) * np.sin((n * np.pi * x) / (a))
+    return y
+
+plt.subplot(2,1,1)
+plt.plot(x,(psi(x,n)),label=rf'$\psi_{n}$')
+plt.xlim(0,a)
+plt.axhline(0,color='black')
+plt.subplot(2,1,2)
+plt.plot(x,(psi(x,n))**2,label=rf'$\psi_{n}$')
+plt.ylabel('$\psi(x)^2$')
+plt.xlabel('x')
+plt.xlim(0,a)
+plt.ylim(bottom=0)
+plt.savefig('graph1.jpg')
+st.image('graph1.jpg')
